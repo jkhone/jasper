330dd70658f0d15648ad3b3fd53187686b75d497
Jasper Khonesavanh <jasperkhonesavanh@Jaspers-MacBook-Pro.local>
2020-05-12 08:58:57 -0700

    Updates

---
+++ precache-manifest.57e79dd4f997ac6cf71bd5742658c655.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4250cacafc039ded267086d2742cd51b",
+    "url": "/jasper/index.html"
+  },
+  {
+    "revision": "eab88ce1731b37e520e0",
+    "url": "/jasper/static/css/2.d3d22e3b.chunk.css"
+  },
+  {
+    "revision": "c6b66638ccfa4ec952a4",
+    "url": "/jasper/static/css/main.61066c37.chunk.css"
+  },
+  {
+    "revision": "eab88ce1731b37e520e0",
+    "url": "/jasper/static/js/2.465c33f6.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/jasper/static/js/2.465c33f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6b66638ccfa4ec952a4",
+    "url": "/jasper/static/js/main.881de392.chunk.js"
+  },
+  {
+    "revision": "91b1c874f9c5fb80f004",
+    "url": "/jasper/static/js/runtime-main.d8b74bcc.js"
+  },
+  {
+    "revision": "62335206072324b56907eab10bd6f329",
+    "url": "/jasper/static/media/csharpgame.62335206.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/jasper/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/jasper/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/jasper/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/jasper/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/jasper/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "0148854c6b03bf00cd955532549fc8c9",
+    "url": "/jasper/static/media/memory.0148854c.png"
+  },
+  {
+    "revision": "51e2eda3c66548cb6085804d1ff951d1",
+    "url": "/jasper/static/media/mint.51e2eda3.png"
+  }
+]);
